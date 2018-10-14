f4d4189d547b5ea365117bbc9b6b4ace6d833f4e
Sebastian Hilger <hi@7talents.de>
2018-10-14 14:39:06 +0200

    Index and Detail field presentation

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,41 @@
 <template>
-    <panel-item :field="field" />
+    <div class="tiptap-detail-field py-4">
+        <truncate
+            clamp="Show More"
+            :length="200"
+            less="Show Less"
+            type="html"
+            :text="field.value"
+        >
+
+        </truncate>
+    </div>
 </template>
 
 <script>
+import truncate from 'vue-truncate-collapsed'
+
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+    components: {
+        truncate,
+    },
 }
 </script>
+
+<style>
+.tiptap-detail-field h1, .tiptap-detail-field h2, .tiptap-detail-field h3, .tiptap-detail-field h4, .tiptap-detail-field h5, .tiptap-detail-field h6 {
+    font-size: 1em;
+    margin-bottom: 0.35em;
+}
+
+.tiptap-detail-field a {
+    color: var(--primary);
+}
+
+.tiptap-detail-field p, ul, ol, blockquote, pre {
+    margin-bottom: 0.7em;
+}
+</style>
+
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,9 +1,43 @@
 <template>
-    <span>{{ field.value }}</span>
+    <div class="tiptap-index-field py-4">
+        <truncate
+            clamp="Show More"
+            :length="200"
+            less="Show Less"
+            type="html"
+            :text="field.value"
+        >
+
+        </truncate>
+    </div>
 </template>
 
+
+
 <script>
+import truncate from 'vue-truncate-collapsed'
+
 export default {
     props: ['resourceName', 'field'],
+
+    components: {
+        truncate,
+    },
 }
 </script>
+
+<style>
+.tiptap-index-field h1, .tiptap-index-field h2, .tiptap-index-field h3, .tiptap-index-field h4, .tiptap-index-field h5, .tiptap-index-field h6 {
+    font-size: 1em;
+    margin-bottom: 0.35em;
+}
+
+.tiptap-index-field a {
+    color: var(--primary);
+}
+
+.tiptap-index-field p, ul, ol, blockquote, pre {
+    margin-bottom: 0.7em;
+}
+</style>
+
