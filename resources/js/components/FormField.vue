9491c21723382df8660ca2e9cd971f773b2040d5
Timothé Pearce <timothe.pearce@gmail.com>
2019-12-30 19:06:17 +0100

    Add the ability to edit a text with <h4>, <h5> and <h6> tags

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -155,7 +155,7 @@ export default {
                     new BulletList(),
                     new CodeBlock(),
                     new HardBreak(),
-                    new Heading({ levels: [1, 2, 3] }),
+                    new Heading({ levels: [1, 2, 3, 4, 5, 6] }),
                     new HorizontalRule(),
                     new ListItem(),
                     new OrderedList(),
