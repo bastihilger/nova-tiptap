26a5da134b50ac84d285f09157c922e643bf8e5b
Sebastian Hilger <hilger@kraenk.de>
2022-04-25 09:07:13 +0200

    fix detail field view

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,13 +1,13 @@
 <template>
-    <panel-item :field="field">
-        <template slot="value">
-            <excerpt :content="field.value" :should-show="field.shouldShow" />
-        </template>
-    </panel-item>
+  <PanelItem :index="index" :field="field">
+    <template #value>
+      <Excerpt :content="field.value" :should-show="field.shouldShow" />
+    </template>
+  </PanelItem>
 </template>
 
 <script>
 export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
+  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
 }
 </script>
