e676813a907a3370029ee341252ffbb73d9bdb8d
Sebastian Hilger <hi@7talents.de>
2022-06-30 11:26:35 +0200

    fix modals

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -1,31 +1,34 @@
 <template>
-    <div
-        class="
-            fixed top-0 left-0
-            w-full h-full
-            z-50 flex items-center justify-center
-        "
-        v-show="isActive"
-    >
-        <div 
-            class="rounded-lg shadow-lg overflow-hidden z-20"
-            style="width: 800px; max-width: 100%"
-        >
-            
-                <slot />
-
-        </div>
-
-        <div 
+    <teleport to="#modals">
+        <div
             class="
-                z-10 absolute top-0 left-0 w-full h-full
-                bg-black
-            "
-            style="opacity: 0.5"
-            @click="hideMethod"
+                fixed top-0 left-0
+                w-full h-full
+                z-50 flex items-center justify-center
+            " 
+            style="z-index: 999"
+            v-show="isActive"
         >
+            <div 
+                class="rounded-lg shadow-lg overflow-hidden z-20"
+                style="width: 800px; max-width: 100%"
+            >
+                
+                    <slot />
+
+            </div>
+
+            <div 
+                class="
+                    z-10 absolute top-0 left-0 w-full h-full
+                    bg-black
+                "
+                style="opacity: 0.5"
+                @click="hideMethod"
+            >
+            </div>
         </div>
-    </div>
+    </teleport>
 </template>
 
 <script>
